a9b3cf4678f81a5ece09305e7b9f4e594e6bc38a
Yufei Fiona Wang <elffiona0203@gmail.com>
2023-02-16 17:38:14 -0500

    trial 1

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -3,28 +3,31 @@ import torch
 import torch.optim
 import torch.nn as nn
 from torchvision.transforms import Compose, Normalize, ToTensor
+from scheduler import CustomLRScheduler
 
 
 class CONFIG:
-
-    batch_size = 64
-    num_epochs = 2
-    initial_learning_rate = 0.001
-    initial_weight_decay = 0
+    batch_size = 80
+    num_epochs = 50
+    initial_learning_rate = 0.5
+    initial_weight_decay = 0.01
 
     lrs_kwargs = {
-        # You can pass arguments to the learning rate scheduler
-        # constructor here.
+        "decay_factor": 0.5,
+        "decay_epochs": 10,
     }
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
-    ] = lambda model: torch.optim.Adam(
-        model.parameters(), lr=CONFIG.initial_learning_rate, weight_decay=CONFIG.initial_weight_decay,
+    ] = lambda model: torch.optim.SGD(
+        model.parameters(),
+        lr=CONFIG.initial_learning_rate,
+        momentum=0.9,
+        weight_decay=CONFIG.initial_weight_decay,
     )
-
     transforms = Compose(
         [
             ToTensor(),
+            Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
         ]
     )
--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -1,24 +1,30 @@
+import math
 from typing import List
-
-from torch.optim.lr_scheduler import _LRScheduler
+from torch.optim.lr_scheduler import _LRScheduler, CosineAnnealingWarmRestarts
 
 
 class CustomLRScheduler(_LRScheduler):
-    def __init__(self, optimizer, last_epoch=-1):
-        """
-        Create a new scheduler.
+    """
+    Implemented Scheduler
+    """
 
-        Note to students: You can change the arguments to this constructor,
-        if you need to add new parameters.
-
+    def __init__(self, optimizer, last_epoch=-1, decay_factor=0.5, decay_epochs=20):
         """
-        # ... Your Code Here ...
+        Create a new scheduler that implements step decay.
+        decay_factor: factor by which the learning rate will be reduced after each decay_epochs
+        decay_epochs: the number of epochs after which the learning rate will be decayed.
+        """
+        self.decay_factor = decay_factor
+        self.decay_epochs = decay_epochs
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
 
     def get_lr(self) -> List[float]:
-        # Note to students: You CANNOT change the arguments or return type of
-        # this function (because it is called internally by Torch)
-
-        # ... Your Code Here ...
-        # Here's our dumb baseline implementation:
-        return [i for i in self.base_lrs]
+        """
+        Compute the new learning rate after each epoch.
+        """
+        new_lr = []
+        for lr in self.base_lrs:
+            new_lr.append(
+                lr * self.decay_factor ** (self.last_epoch // self.decay_epochs)
+            )
+        return new_lr
